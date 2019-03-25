3599276ea93ec812b285b1c586d9e63e7e7ea389
DanielNally <dndanielnally451@gmail.com>
2019-03-25 19:11:34 -0400

    file commit

---
+++ slideshow.js
@@ -0,0 +1,8 @@
+window.onclick = myFunction;
+
+// If the user clicks in the window, set the background color of <body> to yellow
+function myFunction() {
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = "black";
+  document.getElementsByTagName("BODY")[0].style.color = "white";
+  document.getElementsByTagName("HEADER")[0].style.backgroundColor = "linear-gradient(#e66465, #9198e5, black)";
+}
